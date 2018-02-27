b34af7f742f404c8aba19f7b676be07dbae48179
Joe Smith <ckhosra1@yahoo.com>
2018-02-26 18:13:47 -0800

    DCP #5 Added "school" SPs

--- src/create_sps.sql
+++ src/create_sps.sql
@@ -6,3 +6,8 @@
 \i install/sps/SP_DCPDeleteUser.spl
 \i install/sps/SP_DCPGetUser.spl
 \i install/sps/SP_DCPDeactivateUser.spl
+
+\i install/sps/SP_DCPUpsertSchool.spl
+\i install/sps/SP_DCPGetSchool.spl
+
+\i install/sps/SP_DCPGetNextId.spl
