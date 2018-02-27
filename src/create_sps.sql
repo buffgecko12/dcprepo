57bb0e093658d9112861b17a8219b5c62e9dc7fd
Chris Khosravi <ckhosra1@yahoo.com>
2018-02-27 11:06:25 -0800

    DCP #5 Added Delete school SP

--- src/create_sps.sql
+++ src/create_sps.sql
@@ -2,12 +2,21 @@
 -- Install file for SPs
 \i install/sps/SP_DCPUpsertContract.spl
 
+-- User
 \i install/sps/SP_DCPUpsertUser.spl
 \i install/sps/SP_DCPDeleteUser.spl
 \i install/sps/SP_DCPGetUser.spl
 \i install/sps/SP_DCPDeactivateUser.spl
 
+-- School
 \i install/sps/SP_DCPUpsertSchool.spl
+\i install/sps/SP_DCPDeleteSchool.spl
 \i install/sps/SP_DCPGetSchool.spl
 
+-- Class
+\i install/sps/SP_DCPUpsertClass.spl
+\i install/sps/SP_DCPDeleteClass.spl
+\i install/sps/SP_DCPGetClass.spl
+
+-- Other
 \i install/sps/SP_DCPGetNextId.spl
