cf05e2f0867111208c239b827b9881650dc1c19d
Chris Khosravi <ckhosra1@yahoo.com>
2018-03-06 10:48:36 -0800

    Update name references

--- src/load_initial_data.sql
+++ src/load_initial_data.sql
@@ -1,9 +1,9 @@
 -- Create DEACTIVATED lookup entries
-SELECT $DB_NAME$Views.SP_$DB_NAME$UpsertUser (0, 'DEACTIVATED', 'ST', '', '', NULL, NULL, NULL,NULL,0,NULL);
-SELECT $DB_NAME$Views.SP_$DB_NAME$DeactivateUser(0);
+SELECT $DB_NAME$Views.SP_DCPUpsertUser (0, 'DEACTIVATED', 'ST', '', '', NULL, NULL, NULL,NULL,0,NULL);
+SELECT $DB_NAME$Views.SP_DCPDeactivateUser(0);
 
-SELECT $DB_NAME$Views.SP_$DB_NAME$UpsertSchool (0, 'DEACTIVATED', NULL, NULL, NULL);
-SELECT $DB_NAME$Views.SP_$DB_NAME$UpsertClass (0, 0, 'DEACTIVATED');
+SELECT $DB_NAME$Views.SP_DCPUpsertSchool (0, 'DEACTIVATED', NULL, NULL, NULL);
+SELECT $DB_NAME$Views.SP_DCPUpsertClass (0, 0, 'DEACTIVATED');
 
 -- Load NextId initial values
 INSERT INTO $DB_NAME$.NextId (IdType, NextValue) VALUES ('contract', 1);
--- src/load_sample_data.sql
+++ src/load_sample_data.sql
@@ -1,9 +1,25 @@
 -- Load schools
-SELECT * FROM SP_$DB_NAME$UpsertSchool(NULL, 'Guillermo Leon Valencia Colegio (sede integrado)', 'Calle 15A Nro 7 - 48','Duitama','Boyaca');
-SELECT * FROM SP_$DB_NAME$UpsertSchool(NULL, 'Instituto Técnico Industrial Rafael Reyes', 'Carrera 18 # 23-116','Duitama','Boyaca');
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertSchool(NULL, 'Guillermo Leon Valencia Colegio (sede integrado)', 'Calle 15A Nro 7 - 48','Duitama','Boyaca');
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertSchool(NULL, 'Instituto Técnico Industrial Rafael Reyes', 'Carrera 18 # 23-116','Duitama','Boyaca');
 
-SELECT * FROM SP_$DB_NAME$UpsertClass(NULL, 1, '10-03');
-SELECT * FROM SP_$DB_NAME$UpsertClass(NULL, 1, '10-01');
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertClass(NULL, 1, '10-03');
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertClass(NULL, 1, '10-01');
+
+-- Add teacher info + classes
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertTeacher(1,
+	JSONB(
+	'{
+		"deletedclasses": [],
+		"currentclasses": [
+			{"classid":1},
+			{"classid":2}
+		]	
+	}'
+	)
+);
+
+-- Add students
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(2,1);
 
 
 /*
