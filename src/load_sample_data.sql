6ef446fce505c77b0e9b1066dec69b408f930738
Joe Smith <ckhosra1@yahoo.com>
2018-03-07 15:59:28 -0800

    Sample data

--- src/load_sample_data.sql
+++ src/load_sample_data.sql
@@ -36,3 +36,12 @@ SELECT * FROM $DB_NAME$Views.SP_DCPUpsertTeacher(2,
 SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(3,1);
 SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(4,1);
 SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(5,1);
+
+
+-- Add contract
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertContract(
+	NULL,1,'G',1,TSTZRANGE(current_timestamp,current_timestamp + INTERVAL '1' MONTH,'[]'),FALSE,current_timestamp + INTERVAL '14' DAY,NULL,'Some student leader requirements',NULL,NULL,NULL,
+	JSONB('{"currentgoals": [{"goalid": null, "difficultylevel": "M","goaldescription": "Some description","achievedflag": null},{"goalid": null, "difficultylevel": "M","goaldescription": "Some description","achievedflag": null}]}'),
+	JSONB('{"currentrewards": [{"rewardid": null, "difficultylevel": "M","rewarddescription": "Some description"},{"rewardid": null, "difficultylevel": "M","rewarddescription": "Some description"}]}'),
+	JSONB('{"currentparties": [{"partyuserid": 3,"contractrole": "PL"},{"partyuserid": 4,"contractrole": "BL"},{"partyuserid": 5,"contractrole": "PT"}]}')
+);
