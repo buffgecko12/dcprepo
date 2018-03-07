89c17033c1864ce560e0dbdec031258498d63de3
Chris Khosravi <ckhosra1@yahoo.com>
2018-03-07 12:44:06 -0800

    Sample data

--- src/load_sample_data.sql
+++ src/load_sample_data.sql
@@ -33,20 +33,6 @@ SELECT * FROM $DB_NAME$Views.SP_DCPUpsertTeacher(2,
 );
 
 -- Add students
-SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(2,1);
-
-
-/*
--- Create a contract (including participants)
-SELECT $DB_NAME$Views.SP_DCPUpsertContract(
-    NULL, 
-    '903', 
-    1, 
-    CURRENT_TIMESTAMP, 
-    CURRENT_TIMESTAMP + INTERVAL '20' DAY, 
-    CURRENT_TIMESTAMP + INTERVAL '10' DAY, 
-    NULL, NULL, NULL, NULL, NULL, 
-    JSONB('{"participants": [{"userid": 1,"contractrole": "MR","signaturets" : "2017-10-29 12:00:00.000000"},{"userid": 2,"contractrole": "PL","signaturets" : "2017-10-29 12:00:00.000000"},{"userid": 3,"contractrole": "BL","signaturets" : "2017-10-29 12:00:00.000000"},{"userid": 4,"contractrole": "PT","signaturets" : "2017-10-29 12:00:00.000000"},{"userid": 5,"contractrole": "PT","signaturets" : "2017-10-29 12:00:00.000000"},{"userid": 6,"contractrole": "PT","signaturets" : "2017-10-29 12:00:00.000000"},{"userid": 7,"contractrole": "PT","signaturets" : null}]}'),
-    NULL
-);
-*/
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(3,1);
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(4,1);
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(5,1);
