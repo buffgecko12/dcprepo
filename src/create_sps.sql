e7613227781673755a410ef00c09b0b7d2f75887
Chris Khosravi <ckhosra1@yahoo.com>
2018-03-07 12:43:13 -0800

    DCP #5 Added upsert contract SP

--- src/create_sps.sql
+++ src/create_sps.sql
@@ -1,7 +1,4 @@
--- Automate run-time generation of this file (scan SP source files in "sps" directory)
 -- Install file for SPs
-\i install/sps/SP_DCPUpsertContract.spl
-
 -- User
 \i install/sps/SP_DCPUpsertUser.spl
 \i install/sps/SP_DCPDeleteUser.spl
@@ -16,7 +13,6 @@
 \i install/sps/SP_DCPUpsertStudent.spl
 \i install/sps/SP_DCPGetStudent.spl
 
-
 -- School
 \i install/sps/SP_DCPUpsertSchool.spl
 \i install/sps/SP_DCPDeleteSchool.spl
@@ -26,6 +22,9 @@
 \i install/sps/SP_DCPUpsertClass.spl
 \i install/sps/SP_DCPDeleteClass.spl
 \i install/sps/SP_DCPGetClass.spl
+
+-- Contract
+\i install/sps/SP_DCPUpsertContract.spl
 
 -- Other
 \i install/sps/SP_DCPGetNextId.spl
