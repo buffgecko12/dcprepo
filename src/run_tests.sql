e71945086e1cb99c8729695fabfb80209a0db11b
Chris Khosravi <ckhosra1@yahoo.com>
2018-03-07 12:43:59 -0800

    DCP #6 Updated tests

--- src/run_tests.sql
+++ src/run_tests.sql
@@ -6,17 +6,37 @@ SELECT * FROM $DB_NAME$Views.SP_DCPUpsertClass(102, 100, '9-05'); -- Classes
 
 -- Add users
 SELECT * FROM $DB_NAME$Views.SP_DCPUpsertUser (100, 'MyTeacher','TR','Joe','Smith',NULL,'MyPhone','joe@smith.com',NULL,0,NULL);
-SELECT * FROM $DB_NAME$Views.SP_DCPUpsertUser (101, 'MyStudent','ST','Nic','Cage',NULL,'MyPhone','nic@cage.com',NULL,0,NULL);
-SELECT * FROM $DB_NAME$Views.SP_DCPUpsertUser (102, 'MyStudent','ST','Sean','Connery',NULL,'MyPhone','the@besht.com',NULL,0,NULL);
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertUser (101, 'MyStudent1','ST','Nic','Cage',NULL,'MyPhone','nic@cage.com',NULL,0,NULL);
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertUser (102, 'MyStudent2','ST','Sean','Connery',NULL,'MyPhone','the@besht.com',NULL,0,NULL);
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertUser (103, 'MyStudent3','ST','Elsa','Benitez',NULL,'MyPhone1','the@shipoopee.com',NULL,0,NULL);
 
 -- Add teacher info
 SELECT * FROM $DB_NAME$Views.SP_DCPUpsertTeacher(100, JSONB('{"deletedclasses": [],"currentclasses": [{"classid":100},{"classid":101}]}'));
-SELECT * FROM $DB_NAME$Views.SP_DCPUpsertTeacher(100, JSONB('{"deletedclasses": [100,101],"currentclasses": [{"classid":102}]}'));
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertTeacher(100, JSONB('{"deletedclasses": [101],"currentclasses": [{"classid":102}]}'));
 
 -- Add student info
-SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(101,1);
-SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(102,1);
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(101,100);
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(102,100);
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(103,100);
+
+-- Modify student info
 SELECT * FROM $DB_NAME$Views.SP_DCPUpsertStudent(101,0);
+
+-- Add contract
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertContract(
+	100,100,'G',100,TSTZRANGE(current_timestamp,current_timestamp + INTERVAL '1' MONTH,'[]'),current_timestamp + INTERVAL '14' DAY,NULL,'Some student leader requirements',NULL,NULL,NULL,
+	JSONB('{"currentgoals": [{"goalid": null, "difficultylevel": "M","goaldescription": "Some description","achievedflag": null},{"goalid": null, "difficultylevel": "M","goaldescription": "Some description","achievedflag": null}]}'),
+	JSONB('{"currentrewards": [{"rewardid": null, "difficultylevel": "M","rewarddescription": "Some description"},{"rewardid": null, "difficultylevel": "M","rewarddescription": "Some description"}]}'),
+	JSONB('{"currentparties": [{"partyuserid": 101,"contractrole": "PL"},{"partyuserid": 102,"contractrole": "BL"},{"partyuserid": 103,"contractrole": "PT"}]}')
+);
+
+-- Modify contact
+SELECT * FROM $DB_NAME$Views.SP_DCPUpsertContract(
+	100,1,'G',1,TSTZRANGE(current_timestamp,current_timestamp + INTERVAL '1' MONTH,'[]'),current_timestamp + INTERVAL '14' DAY,NULL,'Some REALLY leader requirements','dadada',NULL,NULL,
+	JSONB('{"deletedgoals": [1],"currentgoals": [{"goalid": 2, "difficultylevel": "D","goaldescription": "Some new description"}]}'),
+	JSONB('{"deletedrewards": [1],"currentrewards": [{"rewardid": 2, "difficultylevel": "E","rewarddescription": "Some new description"}]}'),
+	JSONB('{"deletedparties": [101],"currentparties": [{"partyuserid": 103,"contractrole": "PL"}]}')
+);
 
 -- Get school/class info
 SELECT * FROM $DB_NAME$Views.SP_DCPGetSchool(100);
@@ -31,11 +51,13 @@ SELECT * FROM $DB_NAME$Views.SP_DCPGetUser(100,'MyTeacher',NULL);
 SELECT * FROM $DB_NAME$Views.SP_DCPDeactivateUser(101);
 
 -- Delete objects
-SELECT * FROM $DB_NAME$Views.SP_DCPDeleteUser(101);
 SELECT * FROM $DB_NAME$Views.SP_DCPDeleteClass(100);
 SELECT * FROM $DB_NAME$Views.SP_DCPDeleteSchool(100);
 
--- Other
-SELECT * FROM $DB_NAME$Views.SP_DCPGetNextId('class');
+SELECT * FROM $DB_NAME$Views.SP_DCPDeleteUser(100);
+SELECT * FROM $DB_NAME$Views.SP_DCPDeleteUser(101);
+SELECT * FROM $DB_NAME$Views.SP_DCPDeleteUser(102);
+SELECT * FROM $DB_NAME$Views.SP_DCPDeleteUser(103);
 
--- Contracts
+-- Meta
+SELECT * FROM $DB_NAME$Views.SP_DCPGetNextId('class');
