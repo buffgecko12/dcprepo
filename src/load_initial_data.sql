f1a7d021643b6c51ae29732e72e02d9a43892c00
Chris Khosravi <ckhosra1@yahoo.com>
2018-03-08 10:34:42 -0800

    DCP #5 Updated to handle FK contstraints on DELETE

--- src/load_initial_data.sql
+++ src/load_initial_data.sql
@@ -1,9 +1,12 @@
--- Create DEACTIVATED lookup entries
+-- Create DEACTIVATED lookup entries (must be done in correct orders)
+SELECT $DB_NAME$Views.SP_DCPUpsertSchool (0, 'DEACTIVATED', NULL, NULL, NULL);
+SELECT $DB_NAME$Views.SP_DCPUpsertClass (0, 0, 'DEACTIVATED');
+
 SELECT $DB_NAME$Views.SP_DCPUpsertUser (0, 'DEACTIVATED', 'ST', '', '', NULL, NULL, NULL,NULL,0,NULL);
 SELECT $DB_NAME$Views.SP_DCPDeactivateUser(0);
 
-SELECT $DB_NAME$Views.SP_DCPUpsertSchool (0, 'DEACTIVATED', NULL, NULL, NULL);
-SELECT $DB_NAME$Views.SP_DCPUpsertClass (0, 0, 'DEACTIVATED');
+SELECT $DB_NAME$Views.SP_DCPUpsertTeacher(0, JSONB('{"currentclasses": [{"classid":0}]}'));
+SELECT $DB_NAME$Views.SP_DCPUpsertStudent(0, 0);
 
 -- Load NextId initial values
 INSERT INTO $DB_NAME$.NextId (IdType, NextValue) VALUES ('contract', 1);
