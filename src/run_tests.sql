28c9db79f724ff641995d845ae175c2bf7e289d3
Joe Smith <ckhosra1@yahoo.com>
2018-07-10 18:22:04 -0500

    DCP #11 Added pExcludeDraftsFlag parameter

--- src/run_tests.sql
+++ src/run_tests.sql
@@ -70,7 +70,7 @@ SELECT * FROM $DB_NAME$Views.SP_DCPModifyContractParties(
 );
 
 -- Get contract info
-SELECT * FROM $DB_NAME$Views.SP_DCPGetContract(100,NULL,NULL);
+SELECT * FROM $DB_NAME$Views.SP_DCPGetContract(100,NULL,NULL,NULL);
 SELECT * FROM $DB_NAME$Views.SP_DCPGetContractInfo(100);
 SELECT * FROM $DB_NAME$Views.SP_DCPGetContractGoal(100,2,NULL);
 SELECT * FROM $DB_NAME$Views.SP_DCPGetContractGoalReward(100,2,NULL);
