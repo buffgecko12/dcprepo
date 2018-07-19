90b2f77257a01bab4ca07a5937842831029ea022
Joe Smith <ckhosra1@yahoo.com>
2018-07-19 16:45:13 -0500

    DCP #8 Updated cleanup statements

--- src/cleanup.sql
+++ src/cleanup.sql
@@ -4,11 +4,8 @@ FROM pg_stat_activity
 WHERE pg_stat_activity.datname = '$DB_NAME$';
 
 -- Clean up DB objects
-
-
 DROP SCHEMA IF EXISTS $DB_NAME$ CASCADE; -- Store base tables
 DROP SCHEMA IF EXISTS $DB_NAME$views CASCADE; -- Store views, functions, etc. (keep value lower case for PG)
-
 
 DROP DATABASE IF EXISTS $DB_NAME$;
 DROP ROLE IF EXISTS $DB_USER$;
