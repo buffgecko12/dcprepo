b8e8ee3457fb5d6116e0d22b517372e10bdbf424
Joe Smith <ckhosra1@yahoo.com>
2018-07-25 05:52:50 -0500

    DCP #18 Renamed

--- src/create_sps.sql
+++ src/create_sps.sql
@@ -4,7 +4,7 @@
 \i install/sps/SP_DCPDeleteUser.spl
 \i install/sps/SP_DCPGetUser.spl
 \i install/sps/SP_DCPGetUserReputationEvent.spl
-\i install/sps/SP_DCPUpdateUserInfo.spl
+\i install/sps/SP_DCPUpdateUserReputation.spl
 \i install/sps/SP_DCPGetUserBadge.spl
 \i install/sps/SP_DCPDeactivateUser.spl
 
