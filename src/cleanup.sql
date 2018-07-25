59035b40259707526497778c7d8f94bb411b60ef
Joe Smith <ckhosra1@yahoo.com>
2018-07-25 05:58:29 -0500

    DCP #1 Suppress NOTICE messages when dropping objects.

--- src/cleanup.sql
+++ src/cleanup.sql
@@ -5,6 +5,9 @@ FROM pg_stat_activity
 WHERE pg_stat_activity.datname = '$APP_DATABASE$';
 */
 
+-- Suppress "NOTICE" messages
+SET client_min_messages=WARNING;
+
 -- Clean up DB objects
 DROP SCHEMA IF EXISTS $APP_NAME$ CASCADE; -- Store base tables
 DROP SCHEMA IF EXISTS $APP_NAME$Views CASCADE; -- Store views, functions, etc. (keep value lower case for PG)
