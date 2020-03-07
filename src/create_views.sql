0bbb354a22d54f8c3545f51d9f37086e5275e8dd
Chris Khosravi <chris.khosravi@gmail.com>
2020-03-07 17:56:52 +0200

    DCP #1 Updated views
    Renamed "All" views to "Hist" views
    Added UserDisplayName field to Users view

--- src/create_views.sql
+++ src/create_views.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE VIEW $APP_NAME$Views.Object AS SELECT * FROM $APP_NAME$.Object
 
 -- File
 CREATE OR REPLACE VIEW $APP_NAME$Views.File AS SELECT * FROM $APP_NAME$.File WHERE EndTS = TIMESTAMP '9999-12-31 23:59:59';
-CREATE OR REPLACE VIEW $APP_NAME$Views.FileAll AS SELECT * FROM $APP_NAME$.File; -- History rows
+CREATE OR REPLACE VIEW $APP_NAME$Views.FileHist AS SELECT * FROM $APP_NAME$.File; -- History rows
 
 -- Role
 CREATE OR REPLACE VIEW $APP_NAME$Views.Role AS SELECT * FROM $APP_NAME$.Role;
@@ -11,7 +11,7 @@ CREATE OR REPLACE VIEW $APP_NAME$Views.Role_ACL AS SELECT * FROM $APP_NAME$.Role
 
 -- School / class
 CREATE OR REPLACE VIEW $APP_NAME$Views.School AS SELECT * FROM $APP_NAME$.School WHERE SchoolId <> 0 AND EndTS = TIMESTAMP '9999-12-31 23:59:59';
-CREATE OR REPLACE VIEW $APP_NAME$Views.SchoolAll AS SELECT * FROM $APP_NAME$.School WHERE SchoolId <> 0; -- History rows
+CREATE OR REPLACE VIEW $APP_NAME$Views.SchoolHist AS SELECT * FROM $APP_NAME$.School WHERE SchoolId <> 0; -- History rows
 CREATE OR REPLACE VIEW $APP_NAME$Views.School_Calendar AS SELECT * FROM $APP_NAME$.School_Calendar;
 CREATE OR REPLACE VIEW $APP_NAME$Views.School_Reward AS SELECT * FROM $APP_NAME$.School_Reward;
 CREATE OR REPLACE VIEW $APP_NAME$Views.School_Rubric AS SELECT * FROM $APP_NAME$.School_Rubric;
@@ -19,12 +19,12 @@ CREATE OR REPLACE VIEW $APP_NAME$Views.Class AS SELECT * FROM $APP_NAME$.Class W
 
 -- Contracts
 CREATE OR REPLACE VIEW $APP_NAME$Views.Contract AS SELECT * FROM $APP_NAME$.Contract WHERE EndTS = TIMESTAMP '9999-12-31 23:59:59';
-CREATE OR REPLACE VIEW $APP_NAME$Views.ContractAll AS SELECT * FROM $APP_NAME$.Contract;
+CREATE OR REPLACE VIEW $APP_NAME$Views.ContractHist AS SELECT * FROM $APP_NAME$.Contract;
 CREATE OR REPLACE VIEW $APP_NAME$Views.Contract_Party AS SELECT * FROM $APP_NAME$.Contract_Party;
 CREATE OR REPLACE VIEW $APP_NAME$Views.Contract_Party_Reward AS SELECT * FROM $APP_NAME$.Contract_Party_Reward;
 
 -- Users
-CREATE OR REPLACE VIEW $APP_NAME$Views.UsersAll AS SELECT * FROM $APP_NAME$.Users;
+CREATE OR REPLACE VIEW $APP_NAME$Views.UsersHist AS SELECT * FROM $APP_NAME$.Users;
 CREATE OR REPLACE VIEW $APP_NAME$Views.User_Reputation_Event AS SELECT * FROM $APP_NAME$.User_Reputation_Event;
 CREATE OR REPLACE VIEW $APP_NAME$Views.User_Badge AS SELECT * FROM $APP_NAME$.User_Badge;
 CREATE OR REPLACE VIEW $APP_NAME$Views.User_Notification AS SELECT * FROM $APP_NAME$.User_Notification;
@@ -32,9 +32,9 @@ CREATE OR REPLACE VIEW $APP_NAME$Views.User_Profile_Picture AS SELECT * FROM $AP
 
 -- Users
 CREATE OR REPLACE VIEW $APP_NAME$Views.Users AS 
-SELECT UserId, SchoolId, UserName, UserType, FirstName, LastName, EmailAddress, Password, 
-		ReputationValue, ReputationValueLastSeenTS, ProfilePictureId, 
-		Last_Login, Is_Active -- Required for django authentication
+SELECT UserId, SchoolId, UserName, UserType, FirstName, LastName, CAST(FirstName || ' ' || LastName AS VARCHAR(250)) AS UserDisplayName, 
+	   EmailAddress, Password, ReputationValue, ReputationValueLastSeenTS, ProfilePictureId, 
+	   Last_Login, Is_Active -- Required for django authentication
 FROM $APP_NAME$.Users u
 WHERE UserId <> 0 -- Ignore reserve fields
 ;
@@ -43,7 +43,7 @@ CREATE OR REPLACE VIEW $APP_NAME$Views.UsersActive AS SELECT * FROM $APP_NAME$.U
 
 -- Lookup
 CREATE OR REPLACE VIEW $APP_NAME$Views.Reward AS SELECT * FROM $APP_NAME$.Reward WHERE EndTS = TIMESTAMP '9999-12-31 23:59:59';
-CREATE OR REPLACE VIEW $APP_NAME$Views.RewardAll AS SELECT * FROM $APP_NAME$.Reward;
+CREATE OR REPLACE VIEW $APP_NAME$Views.RewardHist AS SELECT * FROM $APP_NAME$.Reward;
 CREATE OR REPLACE VIEW $APP_NAME$Views.Lookup_Event AS SELECT * FROM $APP_NAME$.Lookup_Event;
 CREATE OR REPLACE VIEW $APP_NAME$Views.Lookup_Badge AS SELECT * FROM $APP_NAME$.Lookup_Badge;
 
@@ -55,7 +55,7 @@ FROM $APP_NAME$.Lookup_Profile_Picture;
 
 -- Teacher
 CREATE OR REPLACE VIEW $APP_NAME$Views.Teacher_Program AS SELECT * FROM $APP_NAME$.Teacher_Program WHERE EndTS = TIMESTAMP '9999-12-31 23:59:59';
-CREATE OR REPLACE VIEW $APP_NAME$Views.Teacher_ProgramAll AS SELECT * FROM $APP_NAME$.Teacher_Program;
+CREATE OR REPLACE VIEW $APP_NAME$Views.Teacher_ProgramHist AS SELECT * FROM $APP_NAME$.Teacher_Program;
 CREATE OR REPLACE VIEW $APP_NAME$Views.Teacher_Class AS SELECT * FROM $APP_NAME$.Teacher_Class;
 
 CREATE OR REPLACE VIEW $APP_NAME$Views.Teacher_Program_Info AS 
