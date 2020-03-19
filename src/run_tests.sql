acb62980cef4172f263a0ab1461ea48291697d95
Chris Khosravi <chris.khosravi@gmail.com>
2020-03-19 18:31:59 +0200

    DCP #3 Added Schoolid to file, altered FileCategory

--- src/run_tests.sql
+++ src/run_tests.sql
@@ -8,8 +8,8 @@ SELECT $APP_NAME$Views.SP_DCPUpsertRole(2, 'Some role (school 1)', 'Some descrip
 SELECT $APP_NAME$Views.SP_DCPUpsertRole(3, 'Some role (school 2)', 'Some description', NULL, '{200}', NULL, NULL);
 
 -- Update files
-SELECT $APP_NAME$Views.SP_DCPUpsertFile(100,'MySchoolDataPolicy','.pdf',1000,'pdf','My description','DB',NULL,NULL,NULL,'Other','MS',NULL,2020);
-SELECT $APP_NAME$Views.SP_DCPUpsertFile(200,'MySchoolDataPolicy2','.pdf',1000,'pdf','My description','DB',NULL,NULL,NULL,'Other','MS',NULL,2020);
+SELECT $APP_NAME$Views.SP_DCPUpsertFile(100,'MySchoolDataPolicy','.pdf',1000,'pdf','My description','DB',NULL,NULL,NULL,'Other','MS',NULL,NULL,2020);
+SELECT $APP_NAME$Views.SP_DCPUpsertFile(200,'MySchoolDataPolicy2','.pdf',1000,'pdf','My description','DB',NULL,NULL,NULL,'Other','MS',NULL,NULL,2020);
 
 SELECT $APP_NAME$Views.SP_DCPUpsertRoleACL(2, 200, 'FL', 4, NULL); -- Set role ACL
 
@@ -119,14 +119,14 @@ SELECT $APP_NAME$Views.SP_DCPManageUserDisplayInfo(100,'clearnewrepnotification'
 SELECT $APP_NAME$Views.SP_DCPManageUserDisplayInfo(100,'clearusernotifications',NULL);
 SELECT $APP_NAME$Views.SP_DCPManageUserDisplayInfo(100,'clearusernotifications','BD');
 
-SELECT SP_DCPGetFile(100, NULL, NULL, NULL, NULL);
+SELECT SP_DCPGetFile(100, NULL, NULL, NULL, NULL, NULL);
 
 -- Delete objects
 SELECT $APP_NAME$Views.SP_DCPClearUserNotification(100, NULL, NULL, NULL);
 SELECT $APP_NAME$Views.SP_DCPClearUserNotification(100, NULL, NULL, 'BD');
 SELECT $APP_NAME$Views.SP_DCPDeleteClass(100);
 SELECT $APP_NAME$Views.SP_DCPDeleteSchool(NULL);
-SELECT $APP_NAME$Views.SP_DCPDeleteFile(100, NULL);
+SELECT $APP_NAME$Views.SP_DCPDeleteFile(100, NULL, NULL);
 
 -- Object
 SELECT $APP_NAME$Views.SP_DCPDeleteObject(NULL,NULL);
