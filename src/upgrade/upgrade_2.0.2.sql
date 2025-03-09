23992da1763e5f21a3b91d85777fe9d461abec9d
ravioli <chris.khosravi@gmail.com>
2025-03-08 19:35:21 -0800

    dcp-81 New business objects - gallery projects, gallery photos

---
+++ src/upgrade/upgrade_2.0.2.sql
@@ -0,0 +1,11 @@
+/*
+	Upgrade script steps
+	1. Apply DDL changes
+	2. Re-install logic (views, SPs, UDFs)
+
+	How to Test Upgrade
+	1. Install source version (i.e. 2.0.0)
+	2. Run upgrade script (2.0.0 to 2.0.2)
+
+*/
+
--- versioninfo.py
+++ versioninfo.py
@@ -4,5 +4,5 @@ CODE_VERSION = '2.0.2'
 VERSION_HISTORY = {
     '2.0.0': {'order':1, 'version':'2.0.0', 'repoupgradefile':''},
     '2.0.1': {'order':2, 'version':'2.0.1', 'repoupgradefile':'upgrade/upgrade_2.0.1.sql'},
-#     '2.0.2': {'order':3, 'version':'2.0.2', 'repoupgradefile':'upgrade/upgrade_2.0.2.sql'},
+    '2.0.2': {'order':3, 'version':'2.0.2', 'repoupgradefile':'upgrade/upgrade_2.0.2.sql'},
 }
