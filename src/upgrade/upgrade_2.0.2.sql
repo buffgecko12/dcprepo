b10ae6a4d6d8ada8475fb4bd5de72a06ce6f4d29
Chris Khosravi <chris.khosravi@gmail.com>
2025-03-23 20:31:44 -0700

    DCP-81 Added new program file type for Foto

--- src/upgrade/upgrade_2.0.2.sql
+++ src/upgrade/upgrade_2.0.2.sql
@@ -11,5 +11,14 @@
 
 */
 
--- No schema / metadata changes
 -- Updated sort in SP_DCPGetFileHierarchy
+
+-- Add new file types
+insert into $APP_NAME$.LOOKUP_CATEGORY (CategoryClass, CategoryType, CategoryDisplayName, Description) 
+select 'programfile', 'PHT', 'Foto', null
+where not exists (
+    select 1 
+    from $APP_NAME$.LOOKUP_CATEGORY
+    where CategoryClass = 'programfile'
+    and CategoryType = 'PHT'
+)
